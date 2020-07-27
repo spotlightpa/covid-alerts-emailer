4923224f3148af0e47056334ec36b3013c0d47c9
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-27 15:44:18 -0400

    Add docstring and add optional param

--- src/modules/process_data/merge_geo.py
+++ src/modules/process_data/merge_geo.py
@@ -5,8 +5,23 @@ import logging
 
 
 def merge_geo(
-    gdf: geopandas.GeoDataFrame, data: Dict[str, pd.DataFrame]
+    gdf: geopandas.GeoDataFrame,
+    data: Dict[str, pd.DataFrame],
+    add_per_capita: bool = True,
 ) -> geopandas.GeoDataFrame:
+    """
+    Processes a geopandas geodataframe by merging it with data stored on a dict of pandas dataframes representing
+    cases, deaths, etc.
+
+    Args:
+        gdf (geopandas.GeoDataFrame): geodataframe with boundaries and other important ID fields
+        data (Dict[str, pd.DataFrame]): Dict of pandas dataframes that contains case, deaths, etc. data for merging
+        add_per_capita (bool): Adds a new column for each dataframe in data that represents per capita info. Eg.
+            per_capita_cases, per_capita_deaths. Defaults to True.
+
+    Returns:
+        An updated geopandas geodataframe.
+    """
     logging.info("Merging covid data with geo data...")
     for data_type, df in data.items():
         df = df.drop("Total", axis=1)
@@ -17,10 +32,12 @@ def merge_geo(
         df_most_recent.columns = [col_name]
         gdf = gdf.merge(df_most_recent, how="left", left_on="NAME", right_index=True)
         gdf[col_name] = gdf[col_name].astype("int")
-        col_name_capita = f"{data_type}_per_capita"
-        gdf[col_name_capita] = (
-            gdf[col_name] / gdf["population"]
-        ) * 100000  # calculate rate per 100k people
+
+        if add_per_capita:
+            col_name_capita = f"{data_type}_per_capita"
+            gdf[col_name_capita] = (
+                gdf[col_name] / gdf["population"]
+            ) * 100000  # calculate rate per 100k people
 
     logging.info("Data merged")
     return gdf
