5d9d1501e8ddefcb1f10c1a9c59f56317fb35583
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-28 12:18:58 -0400

    Create CustomLegend

--- src/modules/gen_chart/CustomLegend.py
+++ src/modules/gen_chart/CustomLegend.py
@@ -1,16 +1,28 @@
 from palettable.tableau import Tableau_10
-from typing import List
+from typing import List, Dict
 
 
 class CustomLegend:
-    def __init__(self, labels: List):
-        colors = Tableau_10.hex_colors
+    """Creates a custom legend based on provided labels."""
+
+    def __init__(
+        self, labels: List[str], *, colors: List[str] = None, limit_labels: int = None
+    ):
+        """
+        Args:
+            labels (List[str]): Labels for legend
+            colors (List, optional): List of hex colors used for Legend. Defaults to Tableau_10 if not provided.
+            limit_labels (int, optional): Limits legend to specified number of items. Defaults to None.
+        """
+        labels = labels if not limit_labels else labels[0:limit_labels]
+        colors = colors if colors else Tableau_10.hex_colors
         field_count = len(labels)
         self.colors = colors[0:field_count]
         self.labels = labels
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "<CustomLegend labels:%s colors:%s>" % (self.labels, self.colors)
 
-    def legend(self):
+    def legend(self) -> List[Dict[str, str]]:
+        """Returns a list of dicts representing the legend"""
         return [{"label": x, "color": y} for x, y in zip(self.labels, self.colors)]
--- tests/test_gen_chart.py
+++ tests/test_gen_chart.py
@@ -1,11 +1,12 @@
 import logging
 import pytest
 from definitions import DIR_TESTS_OUTPUT
+from src.modules.gen_chart.CustomLegend import CustomLegend
 from src.modules.gen_chart.multi_line import multi_line
 from src.modules.gen_chart.map_choropleth import map_choropleth
 from altair_saver import save
 from vega_datasets import data
-
+from palettable.colorbrewer.qualitative import Dark2_5
 from src.modules.init.pandas_opts import pandas_opts
 from src.modules.process_data.helper.stack_df import stack_df
 
@@ -31,17 +32,45 @@ def test_map_choropleth(gdf):
         pytest.fail(f"Test fail: {e}")
 
 
+def test_gen_custom_legend():
+    labels = [
+        "dauphin",
+        "cumberland",
+        "york",
+        "lancaster",
+    ]
+    legend_obj = CustomLegend(labels)
+    legend = legend_obj.legend()
+    assert len(legend) == 4
+
+
+def test_gen_custom_legend_limit():
+    labels = [
+        "dauphin",
+        "cumberland",
+        "york",
+        "lancaster",
+    ]
+    legend_obj = CustomLegend(labels, limit_labels=2)
+    legend = legend_obj.legend()
+    print(legend)
+    assert len(legend) == 2
+
+
 def test_multi_line(cases_multi_county, gdf):
-    pandas_opts()
-    print(gdf[["NAME", "NEIGHBORS"]])
-    print(data.stocks())
-    print(cases_multi_county)
     cols = [col for col in cases_multi_county.columns if col != "date"]
     cols = cols[0:5]
     print(cols)
     df = stack_df(cases_multi_county, stackCols=cols, xAxisCol="date")
+    colors = Dark2_5.hex_colors
+
     chart = multi_line(
-        df, x_axis_col="date", y_axis_col="value", category_col="category"
+        df,
+        x_axis_col="date",
+        y_axis_col="value",
+        category_col="category",
+        domain=cols,
+        range_=colors,
     )
     output_path = DIR_TESTS_OUTPUT / "multiline.png"
     save(chart, str(output_path))
