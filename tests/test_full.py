52b664c73b218f8c35081412fd797693eac68841
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-31 10:51:53 -0400

    Tweak subject line

--- tests/test_full.py
+++ tests/test_full.py
@@ -4,5 +4,5 @@ from src.covid_email_alerts import main
 def test_full_single_county(dauphin_county_dict, dauphin_county):
     """ A full program run using just a single county """
     county_name = dauphin_county["name"]
-    custom_subject_line = f"COVID-19 Report TEST: {county_name}"
+    custom_subject_line = f"COVID-19 FULL test"
     main(dauphin_county_dict, custom_subject_line=custom_subject_line)
