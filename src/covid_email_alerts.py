a3b89588029c6ecd3a92f331650febbad18e5501
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-31 10:38:50 -0400

    Add date

--- src/covid_email_alerts.py
+++ src/covid_email_alerts.py
@@ -23,7 +23,7 @@ from src.modules.process_data.process_stats import process_stats
 from src.modules.aws.copy_to_s3 import copy_to_s3
 from src.modules.send_email.count_subscribers import count_subscribers
 from src.modules.send_email.send_email_list import send_email_list
-from src.modules.helper.time import est_now_iso
+from src.modules.helper.time import est_now_iso, est_now_formatted_brief
 from typing import List, Dict
 
 
@@ -107,11 +107,12 @@ def main(
 
         # Send email
         if email_send:
-            subject = (
-                f"COVID-19 Report: {county_name}"
-                if not custom_subject_line
-                else custom_subject_line
-            )
+            if email_send:
+                subject = (
+                    f"COVID-19 Report: {county_name} ({est_now_formatted_brief()})"
+                    if not custom_subject_line
+                    else custom_subject_line
+                )
             logging.info(f"Sending email for {county_name}...")
             send_email_list(html, email_list_id, subject=subject)
             logging.info("...email sent")
