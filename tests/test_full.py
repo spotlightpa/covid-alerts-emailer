a00a78d4b7a051a8f892cd8c9712ba99ce934e9d
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-07-31 15:50:29 -0400

    Add new tests

--- tests/test_full.py
+++ tests/test_full.py
@@ -1,8 +1,11 @@
 from src.covid_email_alerts import main
+import random
 
 
 def test_full_single_county(dauphin_county_dict, dauphin_county):
     """ A full program run using just a single county """
     county_name = dauphin_county["name"]
-    custom_subject_line = f"COVID-19 FULL test"
-    main(dauphin_county_dict, custom_subject_line=custom_subject_line)
+    subject = (
+        f"COVID-19 Report FULL-RUN TEST: {county_name}, {random.randint(0,999999)}"
+    )
+    main(dauphin_county_dict, custom_subject_line=subject)
