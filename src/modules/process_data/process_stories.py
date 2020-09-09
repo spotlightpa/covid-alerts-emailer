70289c8e71b54b2cbacab8564c36ec90cfb6bea7
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-09-09 12:40:34 -0400

    Add UTM params to story URLs

--- src/modules/process_data/process_stories.py
+++ src/modules/process_data/process_stories.py
@@ -1,5 +1,10 @@
 from typing import List, Dict
 
+from src.modules.helper.add_utm_params import add_utm_params
+
 
 def process_stories(stories: List[Dict[str, str]]):
-    return [{"headline": story["title"], "url": story["url"]} for story in stories]
+    return [
+        {"headline": story["title"], "url": add_utm_params(story["url"])}
+        for story in stories
+    ]
